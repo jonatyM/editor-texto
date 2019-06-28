aae67000baf6a1087c1bd111f12c61b717a20e58
jonaty <jonatypereira@gmail.com>
2019-06-28 17:59:15 -0300

    editor electron v1

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+// Plugin do quill criado
+import './plugins/quill_editor.js'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
